b29ec2018099c9c7b6e5b33c2841da18ff670b6f
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-27 18:41:47 -0600

    Add the tests for the list_repo, followers and following methods
    Co-Authored-By: PaulDebevec <paulmdebevec@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,4 +22,38 @@ RSpec.describe User, type: :model do
       expect(admin.admin?).to be_truthy
     end
   end
+
+  describe 'methods' do
+
+    before(:each) do
+      user1_params = {email: 'dumortier.alexis@gmail.com', first_name: 'Alexis', last_name: 'Dumortier', password_digest: 'temp', role: 0, token: ENV['GITHUB_USER1_TOKEN']}
+      @user1 = User.create(user1_params)
+    end
+
+    it "returns repositories" do
+      VCR.use_cassette('/user/returns_repositories') do
+        @user1.list_repositories.each do |repository| 
+          expect(repository).to be_an_instance_of(Repository)
+        end
+      end
+    end
+
+    it "returns followers" do
+      VCR.use_cassette('/user/returns_followers') do
+        @user1.list_followers.each do |follower| 
+          expect(follower).to be_an_instance_of(Follower)
+        end
+      end
+    end
+
+    it "returns following" do
+      VCR.use_cassette('/user/returns_following') do
+        @user1.list_following.each do |following| 
+          expect(following).to be_an_instance_of(Following)
+        end
+      end
+    end
+
+  end 
+    
 end
