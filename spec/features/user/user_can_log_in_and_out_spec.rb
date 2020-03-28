7a9e7b272f5807183a6d9ca024cd4d5b9cb15574
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-27 18:56:28 -0600

    Exit tests that are not relevant with OAuth
    Co-Authored-By: PaulDebevec <paulmdebevec@gmail.com>

--- spec/features/user/user_can_log_in_and_out_spec.rb
+++ spec/features/user/user_can_log_in_and_out_spec.rb
@@ -1,45 +1,49 @@
 require 'rails_helper'
 
 describe 'User' do
-  it 'user can sign in', :vcr do
-    user = create(:user)
+  xit 'user can sign in' do
+    VCR.use_cassette('/user/user_can_sign_in') do
+      user = create(:user)
 
-    visit '/'
+      visit '/'
 
-    click_on "Sign In"
+      click_on "Sign In"
 
-    expect(current_path).to eq(login_path)
+      expect(current_path).to eq(login_path)
 
-    fill_in 'session[email]', with: user.email
-    fill_in 'session[password]', with: user.password
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
 
-    click_on 'Log In'
+      click_on 'Log In'
 
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content(user.email)
-    expect(page).to have_content(user.first_name)
-    expect(page).to have_content(user.last_name)
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(user.email)
+      expect(page).to have_content(user.first_name)
+      expect(page).to have_content(user.last_name)
+    end
   end
 
-  it 'can log out', :js, :vcr do
-    user = create(:user)
+  xit 'can log out', :js do
+    VCR.use_cassette('/user/user_can_sign_out') do
+      user = create(:user)
 
-    visit login_path
+      visit login_path
 
-    fill_in'session[email]', with: user.email
-    fill_in'session[password]', with: user.password
+      fill_in'session[email]', with: user.email
+      fill_in'session[password]', with: user.password
 
-    click_on 'Log In'
+      click_on 'Log In'
 
-    click_on 'Profile'
+      click_on 'Profile'
 
-    expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(dashboard_path)
 
-    click_on 'Log Out'
+      click_on 'Log Out'
 
-    expect(current_path).to eq(root_path)
-    expect(page).to_not have_content(user.first_name)
-    expect(page).to have_content('SIGN IN')
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_content(user.first_name)
+      expect(page).to have_content('SIGN IN')
+    end
   end
 
   it 'is shown an error when incorrect info is entered', :vcr do
