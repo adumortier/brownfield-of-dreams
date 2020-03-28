0d5dbd4d19532644546868e4f06b33b959abcbc8
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-28 12:41:54 -0600

    Add an update method for the sessions controller
    Co-Authored-By: PaulDebevec <paulmdebevec@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,10 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def update
+    user_info = request.env['omniauth.auth']
+    current_user.update_column(:token, user_info[:credentials][:token])
+    redirect_to '/dashboard'
+  end
+
 end
