3f483b27c3324eaa79f937040b7f79c49937cb1d
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-30 16:57:56 -0600

    Test relationship between user, friend and friendship
    Co-Authored-By: PaulDebevec <paulmdebevec@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
   describe 'validations' do
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:first_name)}
     it {should validate_presence_of(:password)}
+  end
+
+  describe 'relationships' do
+    it {should have_many :friendships}
+    it {should have_many(:friends).through(:friendships)}
   end
 
   describe 'roles' do
@@ -26,8 +32,10 @@ RSpec.describe User, type: :model do
   describe 'methods' do
 
     before(:each) do
-      user1_params = {email: 'dumortier.alexis@gmail.com', first_name: 'Alexis', last_name: 'Dumortier', password_digest: 'temp', role: 0, token: ENV['GITHUB_USER1_TOKEN']}
-      @user1 = User.create(user1_params)
+      user1_params = {email: 'dumortier.alexis@gmail.com', first_name: 'Alexis', last_name: 'Dumortier', password: 'temp', role: 0, token: ENV['GITHUB_USER1_TOKEN']}
+      @user1 = User.create! (user1_params)
+      user2_params = {email: 'paul@gmail.com', first_name: 'Paul', last_name: 'Debevec', password: 'temp', role: 0, token: ENV['GITHUB_USER2_TOKEN']}
+      @user2 = User.create!(user2_params)
     end
 
     it "returns repositories" do
@@ -51,6 +59,28 @@ RSpec.describe User, type: :model do
         @user1.list_following.each do |following| 
           expect(following).to be_an_instance_of(Following)
         end
+      end
+    end
+
+    it "returns following" do
+      VCR.use_cassette('/user/returns_username') do
+        expect(@user1.username).to eq('adumortier')
+      end
+    end
+
+    it "returns potential_friends" do
+      VCR.use_cassette('/user/returns_potential_friends') do
+        
+        expect(@user1.potential_friends).to eq({'PaulDebevec' => @user2.id})
+      end
+    end
+
+    it "returns friends" do
+      VCR.use_cassette('/user/returns_friends') do
+        user3_params = {email: 'maria@gmail.com', first_name: 'Maria', last_name: 'Ronauli', password: 'temp', role: 0, token: "asdasdasdasd"}
+        @user3 = User.create!(user3_params)
+        Friendship.create({user_id: @user1.id, friend_id: @user2.id})
+        expect(@user1.list_friends).to eq([@user2])
       end
     end
 
