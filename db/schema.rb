be910c597ece4efe0c1e4338fc66d7aadfa2b10b
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-31 20:09:00 -0600

    Add a status and confirm_token column to the users table
    Co-Authored-By: PaulDebevec <paulmdebevec@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_172326) do
+ActiveRecord::Schema.define(version: 2020_04_01_003621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2020_03_29_172326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.integer "status", default: 0
+    t.string "confirm_token"
     t.index ["email"], name: "index_users_on_email"
   end
 
