d0f7ac0b26856ffcb1b8b6d55e61e992043e5879
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-01 21:16:18 -0600

    Add invites table
    Co-Authored-By: PaulDebevec <paulmdebevec@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_003621) do
+ActiveRecord::Schema.define(version: 2020_04_01_233625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2020_04_01_003621) do
     t.bigint "friend_id"
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.string "github_handle"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
